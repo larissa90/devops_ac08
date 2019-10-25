c39ff8e8e2784ce436d2d6e449550ee7da4c84b1
Anna Flavia de Castro Silva <1901106@salas.aulas>
2019-10-24 22:15:03 -0300

    vai

--- com/faculdadeimpacta/calculadora/test/test_app_web_start.py
+++ com/faculdadeimpacta/calculadora/test/test_app_web_start.py
@@ -1,13 +1,16 @@
+""" Testando Web app """
 from com.faculdadeimpacta.calculadora import app_web_start
 
 
 def test_root_status():
-    instancia_app = app_web_start.app.test_client()
+    """ testando status da raíz """
+    instancia_app = app_web_start.APP.test_client()
     response = instancia_app.get('/')
     assert response.status_code == 200, 'Deveria existir essa rota'
 
 
 def test_root_url():
-    instancia_app = app_web_start.app.test_client()
+    """ testando URL da raíz """
+    instancia_app = app_web_start.APP.test_client()
     response = instancia_app.get('/')
-    assert response.data.decode('utf-8') == 'Index Page', 'Deveria ser...'
+    assert response.data.decode('utf-8') == 'Index Page!', 'Deveria ser Index Page!'
